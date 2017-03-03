9e1845309f89531bb79d243977c33962cd6431e2
michal <mic132@gazeta.pl>
2017-03-03 11:52:18 +0100

    changed classes to ES6

--- src/app.module.js
+++ src/app.module.js
@@ -1,20 +1,21 @@
-(function(app){
-  var NgModule = ng.core.NgModule;
-  var BrowserModule = ng.platformBrowser.BrowserModule;
-  var QuoteService = app.QuoteService;
-  var SecondComponent = app.SecondComponent;
-  var AppComponent = app.AppComponent;
+(function (app) {
+    var NgModule = ng.core.NgModule;
+    var BrowserModule = ng.platformBrowser.BrowserModule;
+    var QuoteService = app.QuoteService;
+    var SecondComponent = app.SecondComponent;
+    var AppComponent = app.AppComponent;
 
-app.AppModule = NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent, SecondComponent], 
-    providers:[
-        {provide: QuoteService, useClass: QuoteService}
-        ],  
-    bootstrap: [AppComponent]
-})
-.Class({
-    constructor: function(){}
-})
+    @NgModule({
+        imports: [BrowserModule],
+        declarations: [AppComponent, SecondComponent],
+        providers: [
+            {provide: QuoteService, useClass: QuoteService}
+        ],
+        bootstrap: [AppComponent]
+    })
+    class AppModule {
+    }
+
+    app.AppModule = AppModule;
 
 })(window.app || (window.app = {}));
